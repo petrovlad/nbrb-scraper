884890f013e9a1e486a254991c3f99b6ecf7d898
petrovlad <vladislav_petrovec@mail.ru>
2023-05-18 10:28:04 +0300

    implement html parsing

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-import requests
 import logging
 from datetime import date, timedelta
 
-# -H 'Accept: */*'
-# -H 'Accept-Language: ru-RU,ru;q=0.9'
-# -H 'Connection: keep-alive'
-# -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8'
-# -H 'Cookie: ASP.NET_SessionId=vl0v4hnmfq0sw0eprqgshnya; _ym_uid=1684478173526043091; _ym_d=1684478173; _ga=GA1.2.554857727.1684478173; _gid=GA1.2.1850794287.1684478173; _ym_isad=2; _ym_visorc=w'
-# -H 'Origin: https://www.nbrb.by'
-# -H 'Referer: https://www.nbrb.by/statistics/rates/ratesdaily.asp'
-# -H 'Sec-Fetch-Dest: empty'
-# -H 'Sec-Fetch-Mode: cors'
-# -H 'Sec-Fetch-Site: same-origin'
-# -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-# -H 'X-Requested-With: XMLHttpRequest'   -H 'sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"'
-# -H 'sec-ch-ua-mobile: ?0'
-# -H 'sec-ch-ua-platform: "Linux"'
-# --data-raw 'Date=2022-05-05&Date=05.05.2022&Type=Day&X-Requested-With=XMLHttpRequest'
-# --compressed
+import requests
+from bs4 import BeautifulSoup
 
 # CONSTS
+SCRAPE_SIZE = 60  # days before
 BASE_URL = 'https://www.nbrb.by/statistics/rates/ratesdaily.asp'
 BASE_HEADERS = {
     'Accept': '*/*',
@@ -55,12 +41,19 @@ def daterange(start_date: date, end_date: date):
 
 
 def main():
-    start_date = date(2023, 5, 1)
-    end_date = date(2023, 5, 25)
+    start_date = date.today()
+    end_date = start_date - timedelta(days=SCRAPE_SIZE)
     for _date in daterange(start_date, end_date):
         raw_data = f'Date={_date.year}-{_date.month}-{_date.day}&Type=Day&X-Requested-With=XMLHttpRequest'
         resp = requests.get(BASE_URL, headers=BASE_HEADERS, data=raw_data)
-        log.debug(f'{resp.status_code}, {resp.json()=}')
+        log.debug(f'{resp.status_code=}, {resp.text=}')
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        currs_list = soup.findChildren('tbody')
+        for curr in currs_list:
+            cur_name = curr.find('td', class_='curName').find('div').find('span').text
+            cur_amount = curr.find('td', class_='curAmount').text
+            cur_course = curr.find('td', class_='curCours').find('div').text
+            print(f'{_date}-{cur_name}-{cur_amount}-{cur_course}')
 
 
 if __name__ == '__main__':
