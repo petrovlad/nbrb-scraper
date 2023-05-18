e72428ee2ff56e35f65ac136cfadcbf9f7c396e5
petrovlad <vladislav_petrovec@mail.ru>
2023-05-18 11:49:37 +0300

    fix plot

--- plot.py
+++ plot.py
@@ -7,12 +7,13 @@ def main():
     db = MySQLDB('localhost', 'aboba', 'aboba', 'nbrb_scraper')
     data = db.get_all()
     grouper_func = lambda _stat: _stat['currency_name']
+    plt.figure(figsize=(20, 14))
     for key, stats_by_key in (groupby(sorted(data, key=grouper_func), key=grouper_func)):
         stats = list(stats_by_key)
         x = [s['date'] for s in stats]
         y = [s['currency_course'] for s in stats]
         plt.plot(x, y, label=f"{stats[0]['currency_name']} ({stats[0]['currency_amount']})")
-    plt.legend(loc='lower left')
+    plt.legend(loc='upper right')
     plt.show()
 
 
